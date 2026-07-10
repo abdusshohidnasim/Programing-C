b21d2e6dc724454a0ec235d1605066e295150507
Abdus Shohid Nasim <abdusshohidnasim@gmail.com>
2026-07-10 16:23:28 +0600

    data type and scanf printf

--- frist_programing.c
+++ frist_programing.c
@@ -1,7 +1,18 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+int main()
+{
     printf("Hello World Hello World");
-   printf(" \t \b \f Naiem\a Hassan Naiem "); 
-    printf("\f Naiem Hassan Naiem"); 
-    getchar(); 
+    printf("\t \b \f Naiem\a Hassan Naiem");
+    printf("\fNaiem Hassan Naiem");
+
+    int a;
+    float b;
+    char c;
+    double d;
+    scanf("%d",&a); 
+    scanf("%f",&b); 
+    scanf(" %c",&c); 
+    scanf("%lf",&d); 
+    printf("a=  %d, b= %f, c = %c d = %lf ", a, b, c, d); 
+    getchar();
 }
