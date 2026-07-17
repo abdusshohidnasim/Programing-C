fb477cb793c62cdf3522ebd9e98250c6d45cf50f
Abdus Shohid Nasim <abdusshohidnasim@gmail.com>
2026-07-17 15:22:05 +0600

    format

--- print.c
+++ print.c
@@ -9,19 +9,15 @@ int main(int argc, char const *argv[])
     printf("%d+%d = %d", a, b, a + b);
     int e = a + b;
     printf("e= %d", e);
-
     // subcirption  ;
     int g = a - b;
     printf("g %d\n", g);
-
     // increment
-    
     printf("a incremetn %d\n", a++);
-
     // decrement
     printf("b decrement %d\n", b--);
     printf("a updated vaou ");
     printf("%d\n", a);
-    printf(" a +=b %d\n",a+=b);
+    printf(" a +=b %d\n", a += b);
     return 0;
 }
