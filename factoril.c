af78c83d1b7eb63a3a2a1646ddf21b06b8146fca
Abdus Shohid Nasim <abdusshohidnasim@gmail.com>
2026-07-17 11:01:27 +0600

    factroila

--- factoril.c
+++ factoril.c
@@ -7,9 +7,18 @@ int main(int argc, char const *argv[])
     printf("Enter your factorila number: ");
     scanf("%d", &factaialnumber);
     printf("your input number %d \n", factaialnumber);
+
+
+     for (factaialnumber; factaialnumber > 1; factaialnumber--)
+        {
+
+            totalnumber = totalnumber * factaialnumber;
+        }
+
+    printf("with out condition %d\n",totalnumber); 
     if (factaialnumber == 0 || factaialnumber == 1)
     {
-        printf("Factoil nimber of if : 1");
+        printf("Factoil nimber of if : 1 %d \n", totalnumber);
     }
     else if (factaialnumber < 1)
     {
@@ -22,7 +31,7 @@ int main(int argc, char const *argv[])
     }
     else
     {
-        printf("factroil numer else : %d\n and input number %d", totalnumber);
+        printf("factroil numer else : %d\n and input number %d\n", totalnumber,factaialnumber);
     }
 
     return 0;
